644eda0a0833971d973526b0e1387878d564c278
Alberto Diaz <contacto@albertodiazsaez.com>
2022-05-29 10:05:30 +0200

    Cards for adress and line of Station Screen

--- lib/screens/station/station_screen.dart
+++ lib/screens/station/station_screen.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -18,17 +19,55 @@ class _StationScreenState extends State<StationScreen> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          children: [
-            Text("Líneas: "),
-            Row(
-              children: <Widget>[
-                for (var line in widget.station.lines!)
-                  LineNumber(line.id, HexColor(line.color), 25)
+        Card(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 10,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 5),
+                  child: Text(
+                    ('station.line').plural(widget.station.lines!.length),
+                    textScaleFactor: 1.2,
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    for (var line in widget.station.lines!)
+                      LineNumber(line.id, HexColor(line.color), 25)
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
+        ),
+        Card(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 10,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  tr('station.adress'),
+                  textScaleFactor: 1.2,
+                ),
+                Text(
+                  widget.station.adress,
+                ),
+              ],
+            ),
+          ),
         )
       ],
     );
