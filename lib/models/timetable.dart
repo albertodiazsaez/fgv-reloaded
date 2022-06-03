300db739364cafe5213acb8667704dc66b3d5a9c
albertodiaz <contacto@albertodiazsaez.com>
2022-06-04 00:05:39 +0200

    Added timetable models.

---
+++ lib/models/timetable.dart
@@ -0,0 +1,37 @@
+import 'package:metrovalencia_reloaded/models/station.dart';
+
+class Timetable {
+  Station originStation;
+  Station destinationStation;
+  String zones;
+  double carbonFootprint;
+  int duration;
+  int distance;
+  List<Transfer> transfers;
+
+  Timetable(
+    this.originStation,
+    this.destinationStation,
+    this.zones,
+    this.carbonFootprint,
+    this.duration,
+    this.distance,
+    this.transfers
+  )
+
+}
+
+class Transfer {
+  Station originStation;
+  Station destinationStation;
+  String possibleDestinations;
+  Map<int, List<String>> departures;
+
+  Transfer(
+    this.originStation,
+    this.destinationStation,
+    this.possibleDestinations,
+    this.departures
+  )
+
+}
